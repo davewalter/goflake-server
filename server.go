53ff99fb5c660a4afeaece37d04f8eccfde7833f
Dave Walter <dwalter@pivotal.io>
2019-11-06 16:36:19 -0800

    Add /healthz endpoint

--- server.go
+++ server.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/bstick12/goflake"
 	"github.com/gorilla/mux"
 )
 
 var generator = goflake.GoFlakeInstanceUsingUnique("D01Z01")
+var started = time.Now()
 
 func main() {
 	startServer()
@@ -20,11 +23,11 @@ func startServer() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/ids", Id)
 	router.Queries("count", "{count:[0-9]+}")
+	router.HandleFunc("/healthz", Health)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Id(w http.ResponseWriter, r *http.Request) {
-
 	values := r.URL.Query()
 	countVar := values["count"]
 	var count = 2
@@ -39,6 +42,18 @@ func Id(w http.ResponseWriter, r *http.Request) {
 	for i := 0; i < count; i++ {
 		ids = append(ids, generator.GetBase64UUID())
 	}
-	json.NewEncoder(w).Encode(ids)
 
+	json.NewEncoder(w).Encode(ids)
+}
+
+func Health(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(started)
+
+	if duration.Seconds() > 10 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("error: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	}
 }
